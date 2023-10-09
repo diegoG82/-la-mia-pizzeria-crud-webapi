bc161593373cc88ce6027003aa08f528ee7e672a
diegoG82 <diegogiardina@hotmail.it>
2023-10-09 14:40:43 +0200

    Inserite Chiamate API con filtro per nome ed Id

--- la-mia-pizzeria-static/API/PizzasController.cs
+++ la-mia-pizzeria-static/API/PizzasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.API
@@ -7,5 +8,38 @@ namespace la_mia_pizzeria_static.API
     [ApiController]
     public class PizzasController : ControllerBase
     {
+        //LISTA DI TUTTE LE PIZZE
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            using PizzaContext db = new PizzaContext();
+            List<Pizza> pizzas = db.Pizzas.ToList();
+
+            return Ok(pizzas);
+        }
+
+        //FILTRO CON NOME
+        [HttpGet]
+        public IActionResult GetPizzasByName(string name)
+        {
+            using PizzaContext db = new PizzaContext();
+            List<Pizza> pizzas = db.Pizzas.Where(p => p.Name.Contains(name)).ToList();
+            return Ok(pizzas);
+
+        }
+
+        //FILTRO CON ID
+        [HttpGet]
+        public IActionResult GetPizzasById(int Id)
+        {
+            using PizzaContext db = new PizzaContext();
+            List<Pizza> pizzas = db.Pizzas.Where(p => p.Id==Id).ToList();
+            return Ok(pizzas);
+
+        }
+
+        //CREAZIONE PIZZA
+
+
     }
 }
