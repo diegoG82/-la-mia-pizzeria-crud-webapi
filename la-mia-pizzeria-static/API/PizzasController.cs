703ab341b5521d1a0245b2b89316e2e759e26859
diegoG82 <diegogiardina@hotmail.it>
2023-10-09 13:24:50 +0200

    Creato controller Api Pizzacontroller

---
+++ la-mia-pizzeria-static/API/PizzasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.API
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+    }
+}
